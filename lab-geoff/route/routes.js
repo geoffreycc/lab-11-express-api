9936f45b769fa5c3712cc8b30837a396c245f5d0
Geoffrey Cox <gcox3d@gmail.com>
2016-12-20 13:35:21 -0800

    testing out post and delete routes

--- lab-geoff/route/routes.js
+++ lab-geoff/route/routes.js
@@ -6,15 +6,22 @@ let storage = require('../lib/storage.js');
 module.exports = (router) => {
   router.get('/movies', (req, res) => {
     console.log('get request');
-    // schemaName, id
-    //stuff
+    storage.getItem('movies', req.url.query.id)
+    .then(movie => {
+      res.json(movie);
+      res.end();
+    });
   });
   router.post('/movies', (req, res) => {
+    if(!req.body) return res.sendStatus(400); //double check message
     let movie = new Movie(req.body.title, req.body.dir, req.body.rating);
     storage.createItem('movies', movie);
   });
   router.delete('/movies', (req, res) => {
-    // schemaName, id
-    //stuff
-  });};
-
+    storage.deleteItem('movies', req.url.query.id);
+    res.end();
+  });
+  // router.put('/movies', (req, res) => {
+  //   //update item
+  // });
+};
