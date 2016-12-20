fcc3a6acab004d1da0ff6e736e97e87361049bd1
Geoffrey Cox <gcox3d@gmail.com>
2016-12-20 11:05:02 -0800

    working on put route. currently not working, no parser

--- lab-geoff/route/routes.js
+++ lab-geoff/route/routes.js
@@ -1,5 +1,8 @@
 'use strict';
 
+let Movie = require('../model/movie.js');
+let storage = require('../lib/storage.js');
+
 module.exports = (router) => {
   router.get('/movies', (req, res) => {
     console.log('get request');
@@ -7,9 +10,8 @@ module.exports = (router) => {
     //stuff
   });
   router.put('/movies', (req, res) => {
-    //movie constructor
-    // schemaName, item
-    //stuff
+    let movie = new Movie(req.body.title, req.body.dir, req.body.rating);
+    storage.createItem('movies', movie);
   });
   router.delete('/movies', (req, res) => {
     // schemaName, id
