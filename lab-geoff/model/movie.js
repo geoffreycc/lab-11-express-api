6c67662794bd6b6adb6c5b08f77340c004f5ed1c
Geoffrey Cox <gcox3d@gmail.com>
2016-12-19 19:55:56 -0800

    created constructor for model

---
+++ lab-geoff/model/movie.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let uuid = require('node-uuid');
+
+module.exports = function(title, dir, rating) {
+  if(!title) throw new Error('title needed');
+  if(!dir) throw new Error('director needed');
+  if(!rating) throw new Error('rating needed');
+  this.id = uuid.v1();
+  this.title = title;
+  this.dir = dir;
+  this.rating = rating;
+};
