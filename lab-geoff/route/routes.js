de6f9b4463980081628042effd937f5d4743820f
Geoffrey Cox <gcox3d@gmail.com>
2016-12-19 23:10:23 -0800

    initial setup for routes. Added dependency injection to server

---
+++ lab-geoff/route/routes.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = (router) => {
+  router.get('/movies', (req, res) => {
+    // schemaName, id
+    //stuff
+  });
+  router.put('/movies', (req, res) => {
+    //movie constructor
+    // schemaName, item
+    //stuff
+  });
+  router.delete('/movies', (req, res) => {
+    // schemaName, id
+    //stuff
+  });};
+
--- lab-geoff/server.js
+++ lab-geoff/server.js
@@ -5,6 +5,10 @@ let express = require('express');
 let PORT = process.env.port || 3000;
 
 let app = express();
+let router = express.Router;
+
+require('./route/routes.js')(router);
+app.use(router);
 
 app.listen(PORT, () => {
   console.log(`Server up on ${PORT}`);
